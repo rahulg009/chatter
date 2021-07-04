0d88f1950ff8471464e7a58da2101b9505e51a69
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-04 23:11:44 +0530

    socket

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const socketio= require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io=socketio
+const io=socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath= path.join(__dirname,'../public')
+
+io.on('connection',()=>{
+    console.log('New Socket Connection')
+})
 
 app.use(express.static(publicDirectoryPath))
 server.listen(port,()=>{
