a5eec053e9c77c687b9eb9e5b58fd363cc7a73da
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-04 20:34:49 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+const path=require("path");
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
-const pulicDirectoryPath= path.join(__dirname,'../public')
+const publicDirectoryPath= path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 app.listen(port,()=>{
-    console.log('Chatter Is Up on ${PORT}')
+    console.log('Chatter Is Up on port ',port)
 })
