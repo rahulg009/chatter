3d4cdce4b58c121b445042cb102159e8a5b1e43e
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-04 20:57:49 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const path=require("path");
+const http=require('http')
 const express = require('express')
+const socketio= require('socket.io')
+
 
 const app = express()
+const server = http.createServer(app)
+const io=socketio
+
 const port = process.env.PORT || 3000
 const publicDirectoryPath= path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('Chatter Is Up on port ',port)
 })
