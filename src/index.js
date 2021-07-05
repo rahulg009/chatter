a2b758f459d5be88e90896a8d9be308d6e8853b9
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-05 10:25:24 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-const path=require("path");
-const http=require('http')
+const path = require('path')
+const http = require('http')
 const express = require('express')
-const socketio= require('socket.io')
-
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io=socketio(server)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath= path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-let count =0;
+app.use(express.static(publicDirectoryPath))
 
-io.on('connection',(socket)=>{
-    console.log('New Socket Connection')
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-    socket.emit('countUpdated',count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment',()=>{
-        count++
-
-        io.emit("countUpdated",count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
-app.use(express.static(publicDirectoryPath))
-server.listen(port,()=>{
-    console.log('Chatter Is Up on port ',port)
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
