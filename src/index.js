abfc89cb9eae935a97a3a7551b8746afed36c4cc
rgdevelopings <rahulgowlapalli01@gmail.com>
2021-07-05 18:49:35 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Chatter App', `${user.username} has left!`))
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
