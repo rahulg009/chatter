a1f27b89193a24a67eccee66ef3cdb57fda2525e
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-07-05 14:27:05 +0530

    Update

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,18 +9,26 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
-    if(!navigator.geolocation){
-        return alert('Geolocation is not supported by your browser!')
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
     }
 
-    navigator.geolocation.getCurrentPosition((position)=>{
-        socket.emit('sendLocation',{
-            latitude:position.coords.latitude,
-            longitude:position.coords.longitude
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')  
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,17 +17,26 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
-    })
-    socket.on('disconnect',()=>{
-        io.emit('message','A user has left!')
+        callback()
     })
 
-    socket.on('sendLocation',(coords)=>{
-        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
