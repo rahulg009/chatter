30f0b8df98d52919bffe78e22a81a0219d26a59c
rgdevelopings <rahulgowlapalli01@gmail.com>
2021-07-06 10:36:19 +0530

    Update

--- public/js/chat.js
+++ public/js/chat.js
@@ -38,7 +38,7 @@ const autoscroll = () => {
 }
 
 socket.on('message', (message) => {
-    console.log(message)
+    // console.log(message)
     const html = Mustache.render(messageTemplate, {
         username: message.username,
         message: message.text,
@@ -49,7 +49,7 @@ socket.on('message', (message) => {
 })
 
 socket.on('locationMessage', (message) => {
-    console.log(message)
+    // console.log(message)
     const html = Mustache.render(locationMessageTemplate, {
         username: message.username,
         url: message.url,
@@ -83,7 +83,7 @@ $messageForm.addEventListener('submit', (e) => {
             return console.log(error)
         }
 
-        console.log('Message delivered!')
+        // console.log('Message delivered!')
     })
 })
 
@@ -100,7 +100,7 @@ $sendLocationButton.addEventListener('click', () => {
             longitude: position.coords.longitude
         }, () => {
             $sendLocationButton.removeAttribute('disabled')
-            console.log('Location shared!')  
+            // console.log('Location shared!')  
         })
     })
 })
